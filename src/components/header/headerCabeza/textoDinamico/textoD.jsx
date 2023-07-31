66dc33fff76a4886581b45bd72761094ca056686
Ezequiel <ezequiel.maureira@gmail.com>
2023-07-31 20:06:55 -0300

    primer state

---
+++ src/components/header/headerCabeza/textoDinamico/textoD.jsx
@@ -0,0 +1,73 @@
+import './textoD.css'
+import { useState, useEffect } from 'react';
+
+const TextoDinamico = () => {
+
+const messages = [
+    'Compra en cuotas online sin interés con visa, mastercard.',
+    'Envío Gratis compras superiores a 10.000.',
+    'Hasta 3 cuotas sin interés en toda la tienda.',
+  ];
+
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [currentMessage, setCurrentMessage] = useState(messages[currentMessageIndex]);
+
+    useEffect(() => {
+   //la funcion useEfectt recibe una fu
+   // Cada vez que se monta el componente, se ejecuta este bloque de código.
+   
+        
+        
+        
+                               //FUNCION SET INTERVAL DE JAVASCRIPT// 
+      /* la funcion setInterval(()=>{},3000) recibe una funcion que es la cual se va a producir
+      y cada cuanto se va a producir y se lo asigna luego de la coma el 3000.que son milisegundos */
+                              
+               const interval = setInterval(() => {
+    
+     
+         
+                       /*esta es la funcion que recibe el set interval
+                       setCurrentMesageIndex, fue la que declaramos arriba que va a actualizar
+                       los estados o hooks del index del arreglo de mensajes  */
+                       
+                       setCurrentMessageIndex((currentMessageIndex) => (currentMessageIndex + 1) % messages.length);
+                                                    },
+            
+            
+                              //los milisegundos          
+                                     1000);
+
+                     return () => clearInterval(interval);
+                  }
+        
+        
+        
+        , []);
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  useEffect(() => {
+    setCurrentMessage(messages[currentMessageIndex]);
+  }, [currentMessageIndex]);
+
+  return (
+              
+        <ul className="nav-links">
+          <li>
+            <h1>{currentMessage}</h1>
+          </li>
+        </ul>
+      
+   
+  );
+};
+
+export default TextoDinamico;
